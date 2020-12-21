4e264313c50d222843b3122c8b4dd06bac2d3ed0
Edward <ma.edward@hotmail.com>
2020-12-20 21:11:09 -0400

    56. Outputting Lists

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -54,14 +54,17 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
+          {this.state.persons.map((person) => {
+            return <Person name={person.name} age={person.age} />;
+          })}
+          {/* <Person
             name={this.state.persons[1].name}
             age={this.state.persons[1].age}
             click={this.switchNameHandler.bind(this, "Max!")}
             changed={this.nameChangedHandler}
           >
             My Hobbies: Racing
-          </Person>
+          </Person> */}
         </div>
       );
     }
