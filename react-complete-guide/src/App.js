6a391751af4f1dcd45c9e01ab24cabbb7f3acedd
Edward <ma.edward@hotmail.com>
2020-12-26 16:51:15 -0400

    71. More on Styled Components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,6 +3,19 @@ import "./App.css";
 import Person from "./Person/Person";
 import styled from "styled-components";
 
+const StyledButton = styled.button`
+background-color: green;
+      color: white;
+      font: inherit;
+      border: 1px solid blue;
+      padding: 8px;
+      cursor: pointer;
+
+      &:hover: {
+        background-color: lightgreen;
+        color: black;
+      }
+`
 class App extends Component {
   state = {
     persons: [
@@ -96,9 +109,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(" ")}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
+        <StyledButton style={style} onClick={this.togglePersonsHandler}>
           Toggle Persons
-        </button>
+        </StyledButton>
         {persons}
       </div>
     );
