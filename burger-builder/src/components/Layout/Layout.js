8910c927a4520fb63f128d82c1caf712d4878e18
Edward <ma.edward@hotmail.com>
2021-02-18 19:27:34 -0400

    143. Adding a Toolbar

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -2,9 +2,12 @@ import React from "react";
 
 import Aux from "../../hoc/Aux";
 import style from "./Layout.module.css";
+
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+
 const layout = (props) => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={style.Content}>{props.children}</main>
   </Aux>
 );
---
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Style from './Toolbar.module.css';
+
+const toolbar = props => (
+    <header className={Style.Toolbar}>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>...</nav>
+    </header>
+);
+
+export default toolbar;
