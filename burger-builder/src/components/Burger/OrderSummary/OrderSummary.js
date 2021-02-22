1e5bc1fb92557a931d778e38a031362449179b45
Edward <ma.edward@hotmail.com>
2021-02-21 23:56:28 -0400

    154. Improving Performance

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Aux from "../../../hoc/Aux";
 import Button from "../../UI/Button/Button";
+
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
     return (
@@ -16,10 +17,16 @@ const orderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A Delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
-      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
+      <p>
+        <strong>Total Price: ${props.price.toFixed(2)}</strong>
+      </p>
       <p>Contiune to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTIUNE</Button>
+      <Button btnType="Danger" clicked={props.purchaseCanceled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTIUNE
+      </Button>
     </Aux>
   );
 };
