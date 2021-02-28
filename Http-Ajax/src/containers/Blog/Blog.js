23b9636f8ec5a4704e3711640389d3258bac07e9
Edward <ma.edward@hotmail.com>
2021-02-28 19:31:18 -0400

    193. Setting Up and Rendering Routes

--- Http-Ajax/src/containers/Blog/Blog.js
+++ Http-Ajax/src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import "./Blog.css";
 
-import Posts from '../Blog/Posts/Posts';
+import Posts from "../Blog/Posts/Posts";
 
 class Blog extends Component {
   render() {
@@ -19,7 +20,8 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Posts />
+        <Route path='/' exact render={()=> <h1>Home</h1>} />
+        <Route path='/' render={()=> <h1>Home 2</h1>} />
       </div>
     );
   }
