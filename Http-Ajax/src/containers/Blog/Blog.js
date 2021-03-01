535bbe98350acab9798ec7cd3c2b0a8e6777a7be
Edward <ma.edward@hotmail.com>
2021-02-28 21:14:56 -0400

    201. Styling the Active Route

--- Http-Ajax/src/containers/Blog/Blog.js
+++ Http-Ajax/src/containers/Blog/Blog.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
 import "./Blog.css";
 
 import Posts from "../Blog/Posts/Posts";
-import NewPost from './NewPost/NewPost';
+import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
   render() {
@@ -13,18 +13,36 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <Link to='/'>Home</Link>
+                <NavLink
+                  to="/"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: "#fa923f",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Home
+                </NavLink>
               </li>
               <li>
-                <Link to={{ pathname: '/new-post', hash: '#submit', search: '?quick-submit=true'}}>New Post</Link>
+                <NavLink
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true",
+                  }}
+                >
+                  New Post
+                </NavLink>
               </li>
             </ul>
           </nav>
         </header>
         {/* <Route path='/' exact render={()=> <h1>Home</h1>} />
         <Route path='/' render={()=> <h1>Home 2</h1>} /> */}
-        <Route path='/' exact component={Posts} />
-        <Route path='/new-post' component={NewPost} />
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
