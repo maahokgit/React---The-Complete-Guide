07dd0a478909881ef7bc81c8c82c904c2b77ed12
Edward <ma.edward@hotmail.com>
2021-03-09 21:48:21 -0400

    267. Combining Multiple Reducers

--- Redux/src/index.js
+++ Redux/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import reducer from "./store/reducer";
 
-const store = createStore(reducer);
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
+import counterReducer from "./store/reducers/counter";
+import resultReducer from "./store/reducers/result";
+
+const rootReducer = combineReducers({
+  ctr: counterReducer,
+  res: resultReducer,
+});
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
