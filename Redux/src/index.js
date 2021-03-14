b3534768c5744b219cb2e50b27213eeb6e517749
Edward <ma.edward@hotmail.com>
2021-03-14 00:28:32 -0300

    285. Using the Redux Devtools

--- Redux/src/index.js
+++ Redux/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 
 import counterReducer from "./store/reducers/counter";
@@ -15,18 +15,23 @@ const rootReducer = combineReducers({
   res: resultReducer,
 });
 
-const logger = store => {
-  return next => {
-    return action => {
-      console.log('[Middleware] Dispatching', action);
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log("[Middleware] Dispatching", action);
       const result = next(action);
-      console.log('[Middleware] next state', store.getState());
-      return result
-      
-    }
-  }
-}
-const store = createStore(rootReducer, applyMiddleware(logger));
+      console.log("[Middleware] next state", store.getState());
+      return result;
+    };
+  };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger))
+);
 
 ReactDOM.render(
   <Provider store={store}>
