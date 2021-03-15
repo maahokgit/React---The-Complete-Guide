63083adcd56eed21db932a67c67e14a692b1c094
Edward <ma.edward@hotmail.com>
2021-03-15 00:17:24 -0300

    293. Using Utility Functions

--- Redux/src/store/reducers/result.js
+++ Redux/src/store/reducers/result.js
@@ -1,5 +1,5 @@
-import * as actionTypes from '../actions/actionsTypes';
-
+import * as actionTypes from "../actions/actionsTypes";
+import { updateObject } from "../utility";
 const initialState = {
   results: [],
 };
@@ -7,16 +7,14 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({ id: new Date(), value: action.result }),
-      };
-      case actionTypes.DELETE_RESULT:
-          const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-      return {
-        ...state,
-        results: updatedArray,
-      };
+      });
+    case actionTypes.DELETE_RESULT:
+      const updatedArray = state.results.filter(
+        (result) => result.id !== action.resultElId
+      );
+      return updateObject(state, { results: updatedArray });
   }
   return state;
 };
