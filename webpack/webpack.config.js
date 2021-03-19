aab4fae6d83268a7f38f91d7732a5978aaa41404
Edward <ma.edward@hotmail.com>
2021-03-19 19:56:17 -0300

    379. Setting Up the Basic Webpack Config

---
+++ webpack/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.export = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "",
+  },
+  devtool: "cheap-module-eval-source-map",
+};
