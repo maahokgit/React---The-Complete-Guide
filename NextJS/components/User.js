798873da58d6da4728eb675cff95716e57af5c74
Edward <ma.edward@hotmail.com>
2021-03-20 02:22:52 -0300

    392. Handling (404) Errors

--- NextJS/components/User.js
+++ NextJS/components/User.js
@@ -5,12 +5,12 @@ const user = (props) => (
     <h1>{props.name}</h1>
     <p>Age: {props.age}</p>
     <style jsx>{`
-        div {
-            border: 1px solid #eee;
-            box-shadow: 0 2px 3px #ccc;
-            padding: 20px;
-            text-align: center;
-        }
+      div {
+        border: 1px solid #eee;
+        box-shadow: 0 2px 3px #ccc;
+        padding: 20px;
+        text-align: center;
+      }
     `}</style>
   </div>
 );
