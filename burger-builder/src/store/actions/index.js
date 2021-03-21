907841ced5c7c19216eb2fae0fe27298c0c778cf
Edward <ma.edward@hotmail.com>
2021-03-21 15:13:04 -0300

    417. Moving More Logic Into Sagas

--- burger-builder/src/store/actions/index.js
+++ burger-builder/src/store/actions/index.js
@@ -6,4 +6,10 @@ export {
 
 export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
 
-export { auth, logout, SetAuthRedirectPath, authCheckState } from "./auth";
+export {
+  auth,
+  logout,
+  SetAuthRedirectPath,
+  authCheckState,
+  logoutSucceed,
+} from "./auth";
