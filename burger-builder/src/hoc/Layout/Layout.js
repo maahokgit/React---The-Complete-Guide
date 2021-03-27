6b95559f074ee51e59f01a057808e35c39b23555
Edward <ma.edward@hotmail.com>
2021-03-26 22:22:20 -0300

    455. Converting the Layout Component

--- burger-builder/src/hoc/Layout/Layout.js
+++ burger-builder/src/hoc/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 
 import Aux from "../Aux/Aux";
@@ -7,39 +7,32 @@ import style from "./Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
-class Layout extends Component {
-  state = {
-    showSideDrawer: false,
+const Layout = props => {
+
+  const [showSideDrawer, setShowSideDrawer] = useState(false);
+
+  const sideDrawerClosedHandler = () => {
+    setShowSideDrawer(false);
   };
 
-  sideDrawerClosedHandler = () => {
-    this.setState({ showSideDrawer: false });
+  const sideDrawerToggleHandler = () => {
+    setShowSideDrawer(!showSideDrawer);
   };
 
-  sideDrawerToggleHandler = () => {
-    this.setState((prevState) => {
-      return {
-        showSideDrawer: !prevState.showSideDrawer,
-      };
-    });
-  };
-
-  render() {
     return (
       <Aux>
         <Toolbar
-          isAuth={this.props.isAuthenticated}
-          drawerToggleClicked={this.sideDrawerToggleHandler}
+          isAuth={props.isAuthenticated}
+          drawerToggleClicked={sideDrawerToggleHandler}
         />
         <SideDrawer
-          isAuth={this.props.isAuthenticated}
-          open={this.state.showSideDrawer}
-          closed={this.sideDrawerClosedHandler}
+          isAuth={props.isAuthenticated}
+          open={showSideDrawer}
+          closed={sideDrawerClosedHandler}
         />
-        <main className={style.Content}>{this.props.children}</main>
+        <main className={style.Content}>{props.children}</main>
       </Aux>
     );
-  }
 }
 
 const mapStateToProps = (state) => {
