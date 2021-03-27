57246c9820ed336c8dd089c4657d74c701b23396
Edward <ma.edward@hotmail.com>
2021-03-27 00:10:11 -0300

    461. Using React.memo() & More!

--- burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import style from "./BurgerIngredient.module.css";
 
 import PropTypes from "prop-types";
 
-class BurgerIngredient extends Component {
-  render() {
+const burgerIngredient = props => {
+
     let ingredient = null;
-    switch (this.props.type) {
+    switch (props.type) {
       case "bread-bottom":
         ingredient = <div className={style.BreadBottom}></div>;
         break;
@@ -35,10 +35,10 @@ class BurgerIngredient extends Component {
     }
 
     return ingredient;
-  }
+
 }
 
-BurgerIngredient.propTypes = {
+burgerIngredient.propTypes = {
   type: PropTypes.string.isRequired,
 };
-export default BurgerIngredient;
+export default burgerIngredient;
