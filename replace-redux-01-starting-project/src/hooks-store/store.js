19379aece28dbab5fd4459cce425c33f06e15984
Edward <ma.edward@hotmail.com>
2021-03-27 22:42:30 -0300

    475. Using the Custom Store

--- replace-redux-01-starting-project/src/hooks-store/store.js
+++ replace-redux-01-starting-project/src/hooks-store/store.js
@@ -1,33 +1,39 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from 'react';
 
 let globalState = {};
 let listeners = [];
 let actions = {};
 
-export const useStore = () => {
+export const useStore = (shouldListen = true) => {
   const setState = useState(globalState)[1];
 
   const dispatch = (actionIdentifier, payload) => {
     const newState = actions[actionIdentifier](globalState, payload);
     globalState = { ...globalState, ...newState };
+
     for (const listener of listeners) {
       listener(globalState);
     }
   };
 
   useEffect(() => {
-    listeners.push(setState);
+    if (shouldListen) {
+      listeners.push(setState);
+    }
+
     return () => {
-      listeners = listeners.filter((li) => li !== setState);
+      if (shouldListen) {
+        listeners = listeners.filter(li => li !== setState);
+      }
     };
-  }, [setState]);
+  }, [setState, shouldListen]);
 
   return [globalState, dispatch];
 };
 
-export const initStore = (userActions, intialState) => {
-  if (intialState) {
-    globalState = { ...globalState, ...intialState };
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
   }
   actions = { ...actions, ...userActions };
 };
--- replace-redux-01-starting-project/src/index.js
+++ replace-redux-01-starting-project/src/index.js
@@ -1,25 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { combineReducers, createStore } from 'redux';
-import ProductsProvider from './context/products-context';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-// import productReducer from './store/reducers/products';
-
-// const rootReducer = combineReducers({
-//   shop: productReducer
-// });
-
-// const store = createStore(rootReducer);
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
